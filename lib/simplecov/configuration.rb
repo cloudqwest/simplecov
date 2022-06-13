79d996325244acbd57852e37131527a2af4b80e4
nitin302 <36194383+nitin302@users.noreply.github.com>
2022-06-13 12:42:12 +0530

    Update configuration.rb

--- lib/simplecov/configuration.rb
+++ lib/simplecov/configuration.rb
@@ -41,7 +41,7 @@ module SimpleCov
       @coverage_path ||= begin
         coverage_path = File.expand_path(coverage_dir, root)
         coverage_path = File.expand_path(coverage_dir, "/tmp") if ENV['COVERAGE_REPORT'] == 'enabled'
-        FileUtils.rm_rf("/tmp/coverage") if ENV['COVERAGE_REPORT'] == 'enabled'
+        #FileUtils.rm_rf("/tmp/coverage") if ENV['COVERAGE_REPORT'] == 'enabled'
         FileUtils.mkdir_p coverage_path
         coverage_path
       end
